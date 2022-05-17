8cdc3a8a3833b4af89668d2a80ebe125c8703f9c
cristian gajardo <cristiang@temuco.cl>
2022-05-17 12:27:36 -0400

    proyecto viable

--- app_root/app/views.py
+++ app_root/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#import inline as inline
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+#from .models import Paciente, Contacto, Atencion
+#from .forms import ContactoForm, PacienteForm, AtencionForm #CustomUserCreationForm   esto es un ModelForm
+from django.forms import inlineformset_factory #esto para inline forms
+from django.contrib import messages #para sweet alert
+from django.contrib.auth import authenticate, login #esto me permite autenticar inmediatamente despues de reigtrar al usuario
+from django.http import Http404
+from django.contrib.auth.decorators import login_required, permission_required
 
-# Create your views here.
+def login(request):
+    return redirect(to='accounts/login/')
+
+@login_required
+def home(request):
+    return render(request, 'app/home.html')
--- app_root/app_site/urls.py
+++ app_root/app_site/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+#---para las imagenes solo en ambiente de DEBUG en produccion se usa nginx-----
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+#para las imagenes solo en ambiente de DEBUG en produccion se usa nginx-----
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
